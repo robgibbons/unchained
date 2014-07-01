43c5495ab38ae28c629d8fd17eb6267ccf820a6f
robgibbons <robgibbons@users.noreply.github.com>
2014-06-30 20:33:54 -0400

    Update index.js

--- views/index.js
+++ views/index.js
@@ -1,23 +1,23 @@
-// Generic URL redirect view
+// Generic URL redirect helper
 exports.redirect = function (url) {
     return function (req, res) {
         res.redirect(url);
     };
 };
 
-// Generic rendered template view
+// Generic template renderer view
 exports.render = function (template) {
     return function (req, res) {
         res.render(template, { params: req.params, user: req.user });
     };
 };
 
-// Same as exports.render() but with login required
+// Same as render() helper, but with login required
 exports.auth = function (template) {
     return [m.requireLogin, view.render(template)];
 };
 
-// Generic error page view (404, etc)
+// Generic error page (404, etc)
 exports.Error = function (req, res) {
     res.render('error', { err_no: req.params.err_no });
 };
